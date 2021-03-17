0df6d5beccec46845680611222197abc7a7c81e2
cerlans <kgomez222@hotmail.com>
2021-03-17 12:16:22 -0400

    3 files modified

--- src/Topics.js
+++ src/Topics.js
@@ -1,112 +1,154 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 // i need to do something here with state, the state needs to moph into a corresponding string based on the div that was clicked, do i have to use an onclick for each and every single one?
-import firebase from 'firebase/app';
-function Topics (){
-  const [query,setQuery] = useState('')
+import firebase from "firebase/app";
+function Topics() {
+  const [query, setQuery] = useState("");
   var user = firebase.auth().currentUser;
 
-function loadClient() {
+  function loadClient() {
     gapi.client.setApiKey("AIzaSyCFiBdff1JxkTe4F_0auryiuqiYMIJd48g");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
+    return gapi.client
+      .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+      .then(
+        function () {
+          console.log("GAPI client loaded for API");
+        },
+        function (err) {
+          console.error("Error loading GAPI client for API", err);
+        }
+      );
   }
   // Make sure the client is loaded before calling this method.
-
   function execute() {
-    return gapi.client.youtube.search.list({
-      "part": [
-        "snippet"
-      ],
-      "maxResults": 10,
-      "q": query,
-      // 'q' well be a template string here and will respond to the div that was clicked, but how do i make an entire div associate with a keyword? its classname? creating a custim 'tag' attribute?
-      "type": [
-        "video"
-      ],
-      "videoDuration": "medium"
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
+    return gapi.client.youtube.search
+      .list({
+        part: ["snippet"],
+        maxResults: 10,
+        q: query,
+        type: ["video"],
+      })
+      .then(
+        function (response) {
+          // Handle the results here (response.result has the parsed body).
+          console.log("Response", response);
+        },
+        function (err) {
+          console.error("Execute error", err);
+        }
+      );
   }
-gapi.load("client",function(){
-    console.log('ive finished loading')
-  });
-  
 
-  return(
+  function loader(){
+    
+     let tester = loadClient().then(function () {
+     execute();
+  });
+  }
+
+  gapi.load("client",function(){
+    console.log('finished loading')
+  });
+  return (
     <>
-      <div className='topics-header'>
+      <div className="topics-header">
+        <button onClick={loadClient}>Load Client</button>
+        <button onClick={execute}>Search</button>
+        <button onClick={loader}>Testing some More</button>
         <h1>What do you want to learn?</h1>
-        {user ? <div>you are signed in</div> : <div> <Link to='/Login'><span className='sign-up'>Sign Up</span></Link> for an account to add tutorials to your subscriptions and take notes!</div>}
+        {user ? (
+          <div>you are signed in</div>
+        ) : (
+          <div>
+            {" "}
+            <Link to="/Login">
+              <span className="sign-up">Sign Up</span>
+            </Link>{" "}
+            for an account to add tutorials to your subscriptions and take
+            notes!
+          </div>
+        )}
       </div>
-      <div className='topics-cont'>
-        <div className='topics-icons'>
-           <div className='topics-card'  style={{background:'#dd4b25'}} >
-            <div className='inner'>
-            <i class="fab fa-html5" style={{color:'white'}}></i>
-            </div> 
-            <div className='text'>
-            <p>HTML5</p>
+      <div className="topics-cont">
+        <div className="topics-icons">
+          <div
+            className="topics-card"
+            onClick={function () {
+              setQuery("html5");
+            }}
+            style={{ background: "#dd4b25" }}
+          >
+            <div className="inner">
+              <i class="fab fa-html5" style={{ color: "white" }}></i>
+            </div>
+            <div className="text">
+              <p>HTML5</p>
             </div>
           </div>
-          <div className='topics-card' style={{background:'#254bdd'}} >
-            <div className='inner'>
-            <i class="fab fa-css3-alt" style={{color:'white'}}></i>
-            </div> 
-            <div className='text'>
-            <p>CSS3</p>
+          <div
+            className="topics-card"
+            onClick={function () {
+              setQuery("css3");
+            }}
+            style={{ background: "#254bdd" }}
+          >
+            <div className="inner">
+              <i class="fab fa-css3-alt" style={{ color: "white" }}></i>
+            </div>
+            <div className="text">
+              <p>CSS3</p>
             </div>
           </div>
-           <div className='topics-card'  style={{background:'#efd81d'}}>
-            <div className='inner'>
-            <i class="fab fa-js" ></i>
-            </div> 
-            <div className='text'>
-            <p>Javascript (ES6)</p>
+          <div
+            className="topics-card"
+            onClick={function () {
+              setQuery("JavaScript");
+            }}
+            style={{ background: "#efd81d" }}
+          >
+            <div className="inner">
+              <i class="fab fa-js"></i>
+            </div>
+            <div className="text">
+              <p>Javascript (ES6)</p>
             </div>
           </div>
-           <div className='topics-card'  style={{background:'#41b883'}}>
-            <div className='inner'>
-            <i class="fab fa-vuejs"></i>
-            </div> 
-            <div className='text'>
-            <p>Vue.Js</p>
+          <div className="topics-card" style={{ background: "#41b883" }}>
+            <div className="inner">
+              <i class="fab fa-vuejs"></i>
+            </div>
+            <div className="text">
+              <p>Vue.Js</p>
             </div>
           </div>
-           <div className='topics-card' style={{background:'#5ed3f3'}}>
-            <div className='inner'>
-            <i class="fab fa-react"></i>
-            </div> 
-            <div className='text'>
-            <p>React.Js</p>
+          <div className="topics-card" style={{ background: "#5ed3f3" }}>
+            <div className="inner">
+              <i class="fab fa-react"></i>
+            </div>
+            <div className="text">
+              <p>React.Js</p>
             </div>
           </div>
-           <div className='topics-card' style={{background:'#8cbf3d'}}>
-            <div className='inner'>
-            <i class="fab fa-node-js"></i>
-            </div> 
-            <div className='text'>
-            <p>Node.Js</p>
+          <div className="topics-card" style={{ background: "#8cbf3d" }}>
+            <div className="inner">
+              <i class="fab fa-node-js"></i>
+            </div>
+            <div className="text">
+              <p>Node.Js</p>
             </div>
           </div>
-           <div className='topics-card' style={{background:'#4584b6'}}>
-            <div className='inner'>
-            <i class="fab fa-python" style={{color:'white'}}></i>
-            </div> 
-            <div className='text'>
-            <p>Python</p>
+          <div className="topics-card" style={{ background: "#4584b6" }}>
+            <div className="inner">
+              <i class="fab fa-python" style={{ color: "white" }}></i>
+            </div>
+            <div className="text">
+              <p>Python</p>
             </div>
           </div>
         </div>
-       </div>
+      </div>
     </>
-  )
+  );
 }
 
-
-export default Topics
+export default Topics;
