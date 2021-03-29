589c276971265bbc170e06237e231f1f4d64a027
cerlans <kgomez222@hotmail.com>
2021-03-29 15:14:21 -0400

    2 files modified, 1 added, 1 deleted

--- src/Tuts.js
+++ src/Tuts.js
@@ -1,37 +1,57 @@
-import React, { useState,useEffect,useRef } from 'react';
-import Test from './Test.js'
-import {useParams} from 'react-router-dom'
-function Tuts(){
-   let { id } = useParams();
-   const [data,setData] = useState([])
-   
-    function execute() {
-    return gapi.client.youtube.search.list({
-      "part": [
-        "snippet"
-      ],
-      "maxResults": 10,
-      "q": id,
-      "type": [
-        "video"
-      ]
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                setData(response.result.items)
-              },
-              function(err) { console.error("Execute error", err); });
+import React, { useState, useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import Listings from "./videoListings.js";
+function Tuts() {
+  let { id } = useParams();
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(null);
+  function execute() {
+    return gapi.client.youtube.search
+      .list({
+        part: ["snippet"],
+        maxResults: 10,
+        q: id,
+        type: ["video"],
+      })
+      .then(
+        function (response) {
+          // Handle the results here (response.result has the parsed body).
+          setLoading(false)
+          setData(response.result.items);
+          
+        },
+        function (err) {
+          console.error("Execute error", err);
+        }
+      );
   }
+  useEffect(() => {
+    execute();
+    setLoading(true);
+  }, []);
   return (
     <>
-      <h1>Testing</h1>
-      {data.map(function(value){
-       console.log(value)
+    {isLoading ? (
+        <div className="loader-parent">
+          <div className="loader"></div>
+        </div>
+      ):
+   ( <div className='listingParent'>
+      {data.map((value) => {
+        return (
+          <Listings
+            title={value.snippet.title}
+            url={value.snippet.thumbnails.medium.url}
+            channel={value.snippet.channelTitle}
+            key={value.etag}
+          />
+        );
       })}
+    
+      </div>
+   )}
     </>
-  )
+  );
 }
 
-
-export default Tuts
-
+export default Tuts;
--- src/videoListings.js
+++ src/videoListings.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 
-function Test(props){
+function Listings(props){
   return(
     <>
-    <div className='container'>
+    <div className='videoListing'>
       <div className='thumbnail'>
         <img src ={props.url}/>
       </div>
       <div className='content'>
-        <h1>{props.title}</h1>
+        <h4>{props.title}</h4>
         <p>{props.channel}</p>
       </div>
     </div>
@@ -17,5 +17,4 @@ function Test(props){
   )
 }
 
-
-export default Test
+export default Listings
