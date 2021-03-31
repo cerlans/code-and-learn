4b1e738ff9ded69b7aafb955813e72c6f09452d6
cerlans <kgomez222@hotmail.com>
2021-03-31 11:11:58 -0400

    added responsive styling to video listings

--- src/videoPlayer.js
+++ src/videoPlayer.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 
 function Player(){
     let { id } = useParams();
+    const [description,setDescription] = useState('')
   console.log(id);
+    function execute() {
+    return gapi.client.youtube.videos.list({
+      "part": [
+        "snippet,contentDetails,statistics"
+      ],
+      "id": [
+        id
+      ]
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response.result.items[0].snippet.description);
+                setDescription(response.result.items[0].snippet.description)
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+  useEffect(()=>{
+    execute()
+  },[])
+
   return (
     <>
+    <div className='iframePanel'>
      <iframe src={`https://www.youtube.com/embed/${id}`}
         frameborder='0'
         allow='autoplay; encrypted-media'
@@ -13,6 +35,10 @@ function Player(){
         title='video'
         width="640" height="390"
      />
+     </div>
+     <div className='videoDescription'>
+     {JSON.stringify(description, null, "\n")}
+     </div>
     </>
   )
 }
